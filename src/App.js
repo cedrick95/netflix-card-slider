f833fac12875a283685d9c68036166df2d3897f0
Creed <elmolangcap@gmail.com>
2018-09-06 13:34:50 +0800

    select active card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      detailState: "active",
+      detailState: false,
+      activeItem: false,
+      items : ""
     }
+
+    this.showDetail = this.showDetail.bind(this);
+    this.closeDetail = this.closeDetail.bind(this);
+  }
+
+  renderItems(){
+    return staticData.map(res => { return (
+      <div key={res.id} className={`items`}>
+        <div className={`item-status-`+`${res.id}`}>
+          {res.id}
+           <i onClick={ () => this.showDetail(res.id) }>
+            <img src={ require( `${res.image}` ) } alt={res.image}/>
+           </i>
+        </div>
+      </div>
+    ) })
+  }
+
+  renderDetail(){
+
   }
 
   nextBtn(){
     document.querySelector(".item-navigation").scrollBy(500,0)
   }
 
-
-
   previousBtn(){
     document.querySelector(".item-navigation").scrollBy(-500,0)
+  }
+
+  showDetail(e){
+    let itemActive = document.querySelector(".item-active");
+    let itemStatus = document.querySelector(`.item-status-`+`${e}`)
+
+    if (itemActive){
+      itemActive.classList.remove("item-active");
+    }
+    itemStatus.classList.add("item-active");
+    this.setState({ detailState: true });
+  }
+
+  closeDetail(){
+    let itemActive = document.querySelector(".item-active");
+    
+    itemActive.classList.remove("item-active")
+    this.setState({ detailState : false})
+  }
+
+  funDetail(){
+    return (
+      <div className="item-details">
+        <button onClick={ this.closeDetail }>
+            close
+        </button>
+      </div>
+    );
   }
 
   render() {
@@ -25,98 +72,21 @@ class App extends Component {
         <div className="items-container ">
           <div className="item-wrapper">
             <div className="item-navigation">
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
-
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
-
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
-
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
-
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
-
-                <div className="items">
-                  <img src={require('./assets/img/card1.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card2.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card3.png')} />
-                </div>
-                <div className="items">
-                  <img src={require('./assets/img/card4.png')} />
-                </div>
+                {this.renderItems()}
+                <div className="last-item"></div>
             </div>
 
             <button className="btn-nav left" onClick={ this.previousBtn }>
-              <img src="http://simpleicon.com/wp-content/uploads/arrow-18.png" height={50} width={50} />
+              <img src="http://simpleicon.com/wp-content/uploads/arrow-18.png" height={50} width={50} alt="left" />
             </button>
 
             <button className="btn-nav right" onClick={ this.nextBtn }>
-              <img src="http://simpleicon.com/wp-content/uploads/arrow-18.png" height={50} width={50} />
+              <img src="http://simpleicon.com/wp-content/uploads/arrow-18.png" height={50} width={50} alt="right" />
             </button>
 
           </div>
 
-          <div className="item-details">
-
-          </div>
+          { this.state.detailState ? this.funDetail() : ""}
 
           </div>
 
@@ -126,10 +96,56 @@ class App extends Component {
 
         <div className="progress-container">
           <div className="progress-bar" id="myBar"></div>
+          {this.renderDetail()}
         </div>
+
+
       </div>
     );
   }
 }
 
 export default App;
+
+const staticData = [
+  {
+    id: 1,
+    image: "./assets/img/card1.png"
+  },
+  {
+    id: 2,
+    image: "./assets/img/card2.png"
+  },
+  {
+    id: 3,
+    image: "./assets/img/card3.png"
+  },
+  {
+    id: 4,
+    image: "./assets/img/card4.png"
+  },
+  {
+    id: 5,
+    image: "./assets/img/card5.png"
+  },
+  {
+    id: 6,
+    image: "./assets/img/card6.png"
+  },
+  {
+    id: 7,
+    image: "./assets/img/card7.png"
+  },
+  {
+    id: 8,
+    image: "./assets/img/card8.png"
+  },
+  {
+    id: 9,
+    image: "./assets/img/card9.png"
+  },
+  {
+    id: 10,
+    image: "./assets/img/card10.png"
+  }
+];
