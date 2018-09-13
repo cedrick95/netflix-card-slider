221df4f7992af6cd69d343a726608b880ec409a2
Creed <elmolangcap@gmail.com>
2018-09-13 14:15:16 +0800

    center active item bug fix

--- src/component/Items.jsx
+++ src/component/Items.jsx
@@ -1,64 +1,48 @@
 import React from 'react';
-import ItemDetail from './Item_detail'
 
 class Items extends React.Component {
   constructor(props){
     super();
     this.state = {
       activeItem: false,
+      itemRef: "",
 
     }
     this.renderItems = this.renderItems.bind(this);
     this.activeItem = this.activeItem.bind(this);
-    this.getItemRef = this.getItemRef.bind(this);
   }
 
-  componentDidUpdate(){
-    const item = {
-      active : document.querySelector(".item-active"),
-    }
-    if(item.active){
-      item.active.scrollIntoView({inline: "center"});
-    }
-  }
-
-
-  clickHandle(e){
-    if(this.state.activeItem == e){
+  clickHandle({ id,ref }){
+    ref.scrollIntoView({inline: "center"})
+    if(this.state.activeItem === id){
       this.setState({ activeItem: false });
-      this.props.activeItem({ activeItem: false , itemId: e });
+      this.props.activeItem({ activeItem: false , itemId: id });
     }
     else{
-      this.props.activeItem({ activeItem: true , itemId: e })
-      this.setState({ activeItem: e });
+      this.props.activeItem({ activeItem: true , itemId: id })
+      this.setState({ activeItem: id });
     }
     this.activeItem;
   }
 
-  activeItem(e){
-      if (this.props.detailVisible && this.state.activeItem == e) {
+  activeItem(id){
+      if (this.props.detailVisible && this.state.activeItem === id) {
         return "item-status item-active";
-      } else if (this.props.detailVisible && this.state.activeItem !== e) {
+      } else if (this.props.detailVisible && this.state.activeItem !== id) {
         return "item-status item-inactive";
       } else {
         return "item-status";
       }
   }
 
-  getItemRef(itemRef){
-    this.setState({ itemRef })
-  }
-
   renderItems(){
-
     if(this.props.dataProps.data){
-      console.log(this.props.dataProps.data);
       return this.props.dataProps.data.map( res =>  {
         let desc = res.field_description[0].value.replace("<p>","").replace("</p>","")
         return (
           <div key={res.nid[0].value} className={`items`}>
-            <div className={ this.activeItem(res.nid[0].value) } ref={this.getItemRef}>
-               <i onClick={ () => this.clickHandle(res.nid[0].value) }>
+            <div className={ this.activeItem(res.nid[0].value) } >
+               <i onClick={ (e) => this.clickHandle({ id: res.nid[0].value, ref : e.target }) } >
                 <div style={{ position: "relative" }}>
                   <img src={ `${res.field_credit_card_image[0].url}` } alt={res.field_credit_card_image[0].url}/>
                   <img className="feature-arrow" src={ require("../assets/icons/show-feature.png")  } alt={res.image}/>
